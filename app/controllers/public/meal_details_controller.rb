017576b5f06e7e45ae6441ce91d063eba9ed8c5e
EC2 Default User <yoshokaaaa@gmail.com>
2022-06-09 14:53:44 +0000

    [delete]meal_details_fields

--- app/controllers/public/meal_details_controller.rb
+++ app/controllers/public/meal_details_controller.rb
@@ -1,14 +1,8 @@
 class Public::MealDetailsController < ApplicationController
   def show
   end
-  
+
   def create
-    
   end
-  
-  private
-  
-  def meal_detail_params
-    permit.require(:meal_detail).permit(:food_id, :meal_id, :quantity)
-  end
+
 end
