4ccc49bb5c4820c2f7c4d2e535c7b472655ff060
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-06-15 13:07:01 +0000

    エラー解決

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :customer_state, only: [:create]
+  before_action :end_user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -34,20 +34,20 @@ class Public::SessionsController < Devise::SessionsController
 
 
   # 退会しているかを判断するメソッド
-  def customer_state
+  def end_user_state
     ## 【処理内容1】 入力されたemailからアカウントを1件取得
-    @customer = Customer.find_by(email: params[:customer][:email])
+    @end_user = EndUser.find_by(email: params[:end_user][:email])
     ## アカウントを取得できなかった場合、このメソッドを終了する
     ## もし@customerがある（!true=false）なら下の処理へ、ない（!false=true）ならreturnして
-    return if !@customer
+    return if !@end_user
     ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致（true）　かつ　is_withdrawalが（false）
-    if @customer.valid_password?(params[:customer][:password]) && !(@customer.is_deleted?)
+    if @end_user.valid_password?(params[:end_user][:password]) && !(@end_user.is_deleted?)
     ## 【処理内容3】（!false=true）だった場合、退会していないのでcreateを実行
     ## 何も書かなくていい
     else
     ## !true（false）だった場合、退会しているのでサインアップ画面に遷移する
     flash[:notice] = "退会済みです。新規会員登録を行なってください"
-    redirect_to new_customer_registration_path
+    redirect_to new_end_user_registration_path
     end
   end
 
