e31dec3ddffd387d1e59787e663b27cbeba26abb
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-06-20 12:17:08 +0000

    プロフィール画像が表示されない

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -36,7 +36,7 @@ class EndUser < ApplicationRecord
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/profile.png')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'app/assets/images/profile.png', content_type: 'image/jpeg')
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
@@ -52,6 +52,7 @@ class EndUser < ApplicationRecord
       user.gender = 0
       user.height = 170
       user.start_weight = 57
+      user.objective_weight = 45
     end
   end
 
