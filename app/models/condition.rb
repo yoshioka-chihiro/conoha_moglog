e39ffb74bc637843643b535afdc4ba9396985857
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-06-22 14:02:53 +0000

    最終調整

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,6 +1,5 @@
 class Condition < ApplicationRecord
   belongs_to :end_user
-  has_many :healths, dependent: :destroy
   has_many :meals, through: :healths
   enum movement: { stone: 0, hard: 1, banana: 2, soft: 3, water: 4 }
   enum feel: { sad: 0, nomal: 1, happy: 2 }
