8106971825be13dda4d6b7d3ff5295b0eb6e4c61
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-06-28 05:03:21 +0000

    完成

--- app/models/diary_comment.rb
+++ app/models/diary_comment.rb
@@ -3,7 +3,8 @@ class DiaryComment < ApplicationRecord
   belongs_to :diary
   has_many :reports, dependent: :destroy
   
-  validates :comment,presence:true,length:{maximum:100}
+
+  validates :comment, presence:true, length:{maximum:100}
 
   def already_reported?(end_user)
    reports.where(end_user_id: end_user.id).exists?
