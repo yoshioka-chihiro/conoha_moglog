bc4b5024cfe8d6db50d7a9b24c89d6ed1dd0f68b
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-07-12 05:52:14 +0000

    otherページのカウント数追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,11 +3,13 @@ class Public::RelationshipsController < ApplicationController
   def create
     current_end_user.follow(params[:end_user_id])
     @end_user = EndUser.find(params[:end_user_id])
+    render 'index'
   end
   # フォロー外すとき
   def destroy
     current_end_user.unfollow(params[:end_user_id])
     @end_user = EndUser.find(params[:end_user_id])
+    render 'index'
   end
   # フォロー一覧
   def followings
