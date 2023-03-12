20647211cc4398154b151a2353a5aa3ba0b4965a
yoshioka-chihiro <yoshokaaaa@gmail.com>
2023-03-12 13:36:19 +0900

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 # server-based syntax
+
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
