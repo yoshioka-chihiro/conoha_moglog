cc71fc69ee7a238d93322ebb65206002d7f82277
yoshioka-chihiro <yoshokaaaa@gmail.com>
2023-03-20 17:37:18 +0900

    gitignore編集

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,8 +1,9 @@
 
+
 #unicornのpidファイル、設定ファイルのディレクトリを指定
 namespace :unicorn do
   task :environment do
-    set :unicorn_pid,    "#{current_path}/shared/tmp/pids/unicorn.pid"
+    set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
     set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
   end
 
